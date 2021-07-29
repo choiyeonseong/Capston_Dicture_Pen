3ce97b936b524d57624153493018d0581c985b4d
YeonSeong✨ <48269478+choiyeonseong@users.noreply.github.com>
2021-07-29 10:01:18 +0900

    Update gui_menu_window.py

--- gui_menu_window.py
+++ gui_menu_window.py
@@ -10,22 +10,20 @@ from gui_date_window import MyApp
 from gui_selectedword_window import *
 from gui_table_window import *
 
-
-
 #메뉴창
-
-
 
 class menuwindow(QMainWindow):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(400, 300)
-
+        
+        # 위 버튼
         self.pushButton = QtWidgets.QPushButton(Dialog)
         self.pushButton.setGeometry(QtCore.QRect(100, 40, 181, 91))
         # self.pushButton.setCheckable(False)
         self.pushButton.setObjectName("pushButton")
-
+        
+        # 아래 버튼
         self.pushButton_2 = QtWidgets.QPushButton(Dialog)
         self.pushButton_2.setGeometry(QtCore.QRect(100, 160, 181, 91))
         # self.pushButton_2.setCheckable(False)
@@ -34,11 +32,11 @@ class menuwindow(QMainWindow):
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-        self.pushButton.clicked.connect(self.my_words)
+        self.pushButton.clicked.connect(self.my_words)  # Word Table로 연결
 
-        self.pushButton_2.clicked.connect(self.camera)
+        self.pushButton_2.clicked.connect(self.camera)  # 카메라 실행
 
-
+    # 표시 이름 설정
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Menu"))
@@ -50,9 +48,6 @@ class menuwindow(QMainWindow):
         self.ex = MyApp()
         self.ex.show()
 
-
-
-
     #pushButton_2
     def camera(self):
         # word, mean = cam_on()
@@ -62,9 +57,6 @@ class menuwindow(QMainWindow):
         self.Window.setWindowTitle("Selected Word")
         self.Window.resize(422, 290)
         self.Window.show()
-
-
-
 
 if __name__=="__main__":
     app=QApplication(sys.argv)
